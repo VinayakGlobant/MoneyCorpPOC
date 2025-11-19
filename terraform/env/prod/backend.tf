7600ea1e6a5886dfd44f5edbd67a9ad3ef61ca12
Vinayak <vinayak.kadam@globant.com>
2025-11-19 13:35:04 +0530

    midifed dashboardjson

--- terraform/env/prod/backend.tf
+++ terraform/env/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"          # pre-create
-    storage_account_name = "sttfstateprod"       # must exist
+    storage_account_name = "stateprodmoneycorp"       # must exist
     container_name       = "tfstate"
     key                  = "dashboard-prod.terraform.tfstate"
   }
